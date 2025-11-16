d4cd18078c30fb0ee804d016d0bdb0c35904771b
elkharchenko-debug <el.kharchenko@1514.ru>
2025-11-16 20:31:54 +0300

    Update change.js

--- 02/change.js
+++ 02/change.js
@@ -1,10 +1,11 @@
 function find_edit(){
     const surname_node = document.getElementById('surname')
-    surname_node.innerHTML=" Kharchenko"
+    surname_node.innerHTML=" Fox"
 
     const name_node = document.getElementById('name')
-    name_node.innerHTML=" Liza"
+    name_node.innerHTML="Silver"
 }
 
 const node_for_click = document.getElementById("for_click")
+
 node_for_click.addEventListener("click",find_edit)
